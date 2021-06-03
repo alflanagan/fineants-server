a473d6fb78788080684720e001e784d7e1b91904
A. Lloyd Flanagan <a.lloyd.flanagan@gmail.com>
2021-06-03 08:33:48 -0400

    begin defining REST API

--- fineants/bofa/views.py
+++ fineants/bofa/views.py
@@ -1,3 +1,29 @@
-from django.shortcuts import render
+from django.urls.conf import include, path
+from rest_framework import routers, serializers, viewsets
 
-# Create your views here.
+from .models import Transaction
+
+
+# Serializers define the API representation.
+class TransactionSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Transaction
+        fields = ["posted_on", "ref_num", "payee", "payee_addr", "amount"]
+
+
+# ViewSets define the view behavior.
+class TransactionViewSet(viewsets.ModelViewSet):
+    queryset = Transaction.objects.all()
+    serializer_class = TransactionSerializer
+
+
+# Routers provide an easy way of automatically determining the URL conf.
+router = routers.DefaultRouter()
+router.register(r"bofa_trans", TransactionViewSet)
+
+# Wire up our API using automatic URL routing.
+# Additionally, we include login URLs for the browsable API.
+urlpatterns = [
+    path("", include(router.urls)),
+    path("api-auth/", include("rest_framework.urls", namespace="rest_framework")),
+]

af69e62943b6fd78338aeb2c5523ae942c169f9b
A. Lloyd Flanagan <a.lloyd.flanagan@gmail.com>
2021-06-02 20:59:53 -0400

    formatted

--- fineants/accounts/models.py
+++ fineants/accounts/models.py
@@ -1,32 +1,36 @@
 from django.db import models
 
-# Create your models here.
-
 
 class Bank(models.Model):
     """A financial institution of some sort."""
+
     name = models.CharField(max_length=200, primary_key=True)
     phone_number = models.CharField(max_length=20)
 
 
 class BankAccount(models.Model):
     bank = models.ForeignKey(
-        Bank, on_delete=models.PROTECT, help_text="Financial institution holding the account")
+        Bank,
+        on_delete=models.PROTECT,
+        help_text="Financial institution holding the account",
+    )
     account_number = models.CharField(max_length=200)
 
     class Meta:
-        unique_together = ('bank', 'account_number')
+        unique_together = ("bank", "account_number")
 
 
 class Transaction(models.Model):
     """Anything that changes the balance of an account."""
+
     account = models.ForeignKey(BankAccount, on_delete=models.PROTECT)
-    amount = models.DecimalField(max_digits=15,
-        decimal_places=2)  # some accounts may require 4?
+    amount = models.DecimalField(
+        max_digits=15, decimal_places=2
+    )  # some accounts may require 4?
     to_from = models.CharField(max_length=100)
     posted = models.DateTimeField()
     memo = models.TextField()
     is_debit = models.BooleanField(null=False)
 
     class Meta:
-        unique_together = ('account', 'posted')
+        unique_together = ("account", "posted")

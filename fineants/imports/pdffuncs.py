c7d4aeeeaaf4ee1ec9ed1a797641036bed6da87d
A. Lloyd Flanagan <a.lloyd.flanagan@gmail.com>
2021-06-06 12:37:09 -0400

    make get_text() return array of strings

--- fineants/imports/pdffuncs.py
+++ fineants/imports/pdffuncs.py
@@ -2,7 +2,8 @@ import argparse
 from typing import Iterable
 
 from pdfminer.high_level import extract_pages
-from pdfminer.layout import LTTextBoxHorizontal
+from pdfminer.layout import LTTextBoxHorizontal, LTContainer, LTTextLine, LTPage
+
 
 # keeping this for now as example of lower-level parsing. better way is below it
 # def get_text(fd: open) -> str:
@@ -17,17 +18,17 @@ from pdfminer.layout import LTTextBoxHorizontal
 #     return output_string.getvalue()
 
 
-def get_text(fd: open) -> Iterable[str]:
+def get_text(pdf_file: open) -> Iterable[str]:
     """
     Reads the open PDF file and returns an Iterable of the strings found.
 
-    Does not close `fd`!
+    Does not close `pdf_file`!
     """
     lines = []
-    pages = extract_pages(opts.pdf_file)
-    for page_layout in pages:
-        for element in page_layout:
+    pages = extract_pages(pdf_file)
+    for page_layout in pages:  # type: LTPage
+        for element in page_layout:  # type: LTContainer
             if isinstance(element, LTTextBoxHorizontal):
-                for line in element:
-                    lines.append(line)
+                for line in element:  # type: LTTextLine
+                    lines.append(line.get_text())
     return lines
